4e8c045c83d2f52ca37897b7e315c8f614e4081e
Tianen <liutianen@outlook.com>
2020-05-13 15:55:01 +0800

    删除调试信息

--- ApkInstall/StartCase.py
+++ ApkInstall/StartCase.py
@@ -1,14 +1,4 @@
-"""
-运行逻辑（暂定）
-版本控制功能，区分TV和手机设备
-1为手机
-2为TV
-整体逻辑是：选择版本后，先进行卸载操作，然后安装
-等待2分钟，或者隐式等待（APP列表出现指定包名）
-启动APP，启动top，启动logcat
-然后等待一段时间后，重启APP
-然后卸载，重装，再运行
-"""
+# coding=utf-8
 from Case.MobileCase import MobileCase
 from Case.TvCase import TvCase
 from Base.Common import Common
@@ -22,15 +12,12 @@ class startInit:
             i = str(input("选择平台(1、手机，2、TV，3、断开无线ADB连接，4、重启ADB服务）："))
             if i == '1':
                 startInit().mobileCase()
-                # print("1")
                 break
             elif i == '2':
                 startInit().tvCase()
-                # print("2")
                 break
             elif i == '3':
                 Common().adbDisconnect()
-                # print("3")
                 break
             elif i == '4':
                 Common().adbRestart()
